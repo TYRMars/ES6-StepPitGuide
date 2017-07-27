47f131b1cb71d2c26ef61c874108a9ee6efb13a4
张佳楠 <zhangjianan1996@qq.com>
2017-07-27 23:12:08 +0800

    do_es6

--- es6/tasks/scripts.js
+++ es6/tasks/scripts.js
@@ -10,3 +10,36 @@ import rename from 'gulp-rename';
 import gulify from 'gulp-uglify';
 import {log,colors} from 'gulp-util';
 import args from './util/args';
+
+gulp.task('scripts',()=>{
+  return gulp.src(['app/js/index.js'])
+  .pipe(plumber({
+    errorHandle:function(){
+
+    }
+  }))
+  .pipe(named())
+  .pipe(gulpWebpack({
+    model:{
+      loaders:[{
+        test:/\.js$/,
+        loader:'babel'
+      }]
+    }
+  }),null,(err,stats)=>{
+    log(`Finished ${colors.cyan('scripts')}'`,st.toString({
+      chunks:false
+    }))
+  })
+  //路径确定
+  .pipe(gulp.dest('server/public/js'))
+  //重命名
+  .pipe(rename({
+    basename:'cp',
+    extname:'.min.js'
+  }))
+  //文件压缩
+  .pipe(uglify({compress:{properties:false},output:{'quote_keys':true}}))
+  .pipe(gulp.dest('server/public/js'))
+  .pipe(gulpif(args.watch,livereload()));
+})
